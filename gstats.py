ed4d66a809cad7b5564c1cb51b06c4eb271be9d4
Hamman Samuel <hwsamuel@ualberta.ca>
2017-06-10 20:01:09 +0000

    gstats.py created online with Bitbucket

---
+++ gstats.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from pymongo import MongoClient
+from province import Province, Provinces
+import re
+
+client = MongoClient()
+BSIZE = 100000
+
+def all_tweets():
+    return client.grebe.tweets.find().batch_size(BSIZE)
+
+def tweets_with_coordinates():
+    return client.grebe.tweets.find({'coordinates.coordinates': {'$exists':True}}).batch_size(BSIZE)
+
+def tweets_in_province(province):
+    province = re.compile(province.value.name+"$", re.I)
+    return client.grebe.tweets.find({'coordinates.coordinates': {'$exists':True}, "place.full_name": {'$regex': province}}).batch_size(BSIZE)
+
+def main():
+    print 'Total Tweets: '+str(all_tweets().count())
+    print 'Tweets with coordinates: '+str(tweets_with_coordinates().count())
+    
+    provinces = [Provinces.AB, Provinces.SK, Provinces.BC]
+    for province in provinces:
+        tweets = tweets_in_province(province)
+        print 'Tweets from ' + province.value.name + ': ' + str(tweets.count())
+
+#main()
