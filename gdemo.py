973d441285963c1bc5c22688ae2b76ec7ad5a774
Hamman <hwsamuel@ualberta.ca>
2017-09-04 13:18:19 +0800

    Updates to hashtag usage

--- gdemo.py
+++ gdemo.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from province import Province, Provinces
 from gexport import *
 from gstats import *
-import sys, pickle
+import sys, pickle, os.path, re, operator
 
 HOME_DIR = '/home/ubuntu/.cache/grebe/' 
 #HOME_DIR = 'F:/PhD/Grebe/source/.cache/grebe/'
@@ -22,8 +22,8 @@ def demo_tweets(start, end, partition = 5):
 def demo_data():
     demo_cache = HOME_DIR + "demo_data.p"
     num_dates = MAX_DATE_RANGE
-    #base = datetime.now()
-    base = datetime.strptime('26-7-2016','%d-%m-%Y')
+    base = datetime.now()
+    #base = datetime.strptime('26-7-2016','%d-%m-%Y')
     tweets = []
     end_date = None
     start_date = None
@@ -47,9 +47,40 @@ def demo_stats():
     qc_tweets = tweets_in_province(Provinces.QC)
     counts = [num_tweets,coord_tweets,ab_tweets,sk_tweets,bc_tweets,mb_tweets,on_tweets,qc_tweets]
     pickle.dump(counts, open(stats_cache, "wb"))
+
+def demo_tags():
+    top_cache = HOME_DIR + "top_tags.p"
+    demo_cache = HOME_DIR + "demo_data.p"
+    if os.path.isfile(demo_cache):
+        demo_tweets = pickle.load(open(demo_cache, "rb" ))
+    else:
+        return
+
+    f = open("glasgow")
+    stop_words = [l.strip() for l in f.readlines()]
+    
+    dict = {}
+    for tweet in demo_tweets:
+        tokens = re.compile("[^a-zA-Z0-9'#]").split(tweet[0])
+        for t in tokens:
+            t = t.lower().strip()
+            if '#' not in t:
+                continue
+            if t in stop_words or len(t) <= 2:
+                continue
+            if t in dict:
+                dict[t] += 1
+            else:
+                dict[t] = 1
+    dict = {k:v for k,v in dict.items() if v > 1}
+    tw = sorted(dict.items(), key=operator.itemgetter(1))[:10]
+    ret = []
+    for k,v in tw:
+        ret.append(k)
+    pickle.dump(ret, open(top_cache, "wb"))
     
 if __name__ == "__main__":
-    usage = '\nUsage: python gdemo.py [data | stats]'
+    usage = '\nUsage: python gdemo.py [data | stats | tags]'
     if len(sys.argv) != 2:
         print usage
     else:
@@ -60,5 +91,8 @@ if __name__ == "__main__":
         elif mode == 'stats':
             demo_stats()
             print "Grebe stats updated"
+        elif mode == 'tags':
+            demo_tags()
+            print "Grebe top hashtags updated"
         else:
             print usage
